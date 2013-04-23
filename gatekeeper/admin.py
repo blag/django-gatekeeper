24199ccca31f57bdb5f07ff3fe977071563dfb6c
Joshua Jonah <joshua@Joshua-Linux.(none)>
2013-04-23 11:41:26 -0400

    Added object information in admin.

--- gatekeeper/admin.py
+++ gatekeeper/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
+from django.template.loader import render_to_string
+
 from gatekeeper.models import ModeratedObject
 
+
 class ModeratedObjectAdmin(admin.ModelAdmin):
-    list_display = ('object_name', 'timestamp', 'moderation_status', 'flagged')
-    list_editable = ('moderation_status','flagged')
-    list_filter = ['moderation_status','flagged',]
+    list_display = ('object_name', 'timestamp', 'moderation_status', 'flagged', 'long_desc')
+    list_editable = ('moderation_status', 'flagged')
+    list_filter = ['moderation_status', 'flagged']
+
+    def long_desc(self, obj):
+        return "%s" % render_to_string('moderation/long_desc.html', {'obj': obj.content_object})
+    long_desc.short_description = 'Object Description'
+    long_desc.allow_tags = True
 
     def object_name(self, obj):
         return "%s" % obj
